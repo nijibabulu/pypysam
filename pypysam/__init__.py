210e19d4ef0a49f5df62573a2f810a955f2f2690
Bob Zimmermann <robert.zimmermann@univie.ac.at>
2016-08-24 20:45:41 +0200

    Adding writing features; some optimizations

--- pypysam/__init__.py
+++ pypysam/__init__.py
@@ -2,7 +2,8 @@
 
 import os
 import types
-import pprint
+import string
+import itertools
 import struct
 import collections
 import bgzf
@@ -160,47 +161,41 @@ class FastaFile(object):
                 s.length += 1
 
 
-_cram_magic = b"\x43\x52\x41\x4d"
-_sam_magic = b"\x40\x48\x44\x20"
 
 class AlignmentReference(object):
     def __init__(self,tid,name,length):
         self.tid = tid
         self.name = name
         self.length = length
+    def __cmp__(self,other):
+        return cmp(self.tid,other.tid) or cmp(self.name,other.name) or cmp(self.length,other.length)
 
 class AlignmentFileBase(object):
-    def __init__(self,filepath):
+    def __init__(self,filepath,mode='r',template=None,header=None,**kwargs):
         self.filepath = filepath
+        self.mode = mode
         self.is_bam = False
         self.is_cram = False
         self.is_sam = False
+        self.write_mode = False
         self.reference_table = collections.OrderedDict()
         self.reference_names = []
-        self.read_header()
-
-class cached_property(object):
-    """ A property that is only computed once per instance and then replaces
-        itself with an ordinary attribute. Deleting the attribute resets the
-        property.
-
-        Source: https://github.com/bottlepy/bottle/commit/fa7733e075da0d790d809aa3d2f53071897e6f76
-        """
-
-    def __init__(self, func):
-        self.__doc__ = getattr(func, '__doc__')
-        self.func = func
-
-    def __get__(self, obj, cls):
-        if obj is None:
-            return self
-        value = obj.__dict__[self.func.__name__] = self.func(obj)
-        return value
+        if mode[0] == 'r':
+            self.read_header()
+        elif mode[0] == 'w':
+            if template is not None:
+                self.header_from_template(template)
+            elif header is not None:
+                self.header_from_object(header)
+            self.write_header()
+            self.write_mode = True
+        else:
+            raise ValueError, 'Unknown mode %s' % mode
 
 class AlignedSegment(object):
     _cigar_ops = ['M','I','D','N','S','H','P','=','X']
     def __init__(self,aln_file,tid,pos,mapq,flag,mate_tid,mate_pos,
-                 tlen,read_name,cigar,seq,qual,tags):
+                 tlen,read_name,cigar,seq,qual,tags,block=None):
         self.qname = read_name
         self.tid = tid
         self.pos = pos
@@ -213,11 +208,14 @@ class AlignedSegment(object):
         self.read_name = read_name
         self.cigar = cigar
         self.qual = qual
-        self.tags = {t.tag:t for t in tags}
+        self.tags = tags
+        self.block = block
+        self._block_format = aln_file._magic
         self._aln_file = aln_file
 
     def has_tag(self,tag_name):
         return tag_name in self.tags
+
     @property
     def query_length(self):
         return len(self.seq)
@@ -313,30 +311,72 @@ class Tag(object):
     def __init__(self,tag,val_type,value):
         self.tag = tag
         self.val_type = val_type
-        self.value = value
+        self._value = value
+
+    @property
+    def value(self):
+        return self._value
+
+class SamTag(Tag):
+    _tag_sam_types = 'cCsSiIAf'
+    _tag_pystruct_types = 'bBhHiIcf'
+    _tag_trans = string.maketrans(_tag_sam_types,_tag_pystruct_types)
+
+    def __init__(self,tag,val_type,value=None,value_block=None):
+        super(SamTag,self).__init__(tag,val_type,value)
+        self._value_block = value_block
+        if value is None and value_block is None:
+            raise ValueError, 'Tag requires either a value or a value block'
+
+    @property
+    def value(self):
+        if self._value is None:
+            if self.val_type == 'Z':
+                self._value = self._value_block
+            elif self.val_type in self._tag_sam_types:
+                self._value = struct.unpack(
+                    self.val_type.translate(self._tag_trans),self._value_block)[0]
+            else:
+                raise NotImplementedError, 'Tag type %s not implemented' % val_type
+        return self._value
+
+    @property
+    def value_block(self):
+        if self._value_block is None:
+            self._value_block = struct.pack(
+                tag.val_type.translate(self._tag_trans),self._value)
+        return self._value_block
+
 
 class SamFile(AlignmentFileBase):
-    def read_header(cls,obj):
-        raise NotImplementedError, 'Sam parsing is not implemented yet'
+    _magic = b"\x40\x48\x44\x20"
+    def __init__(self):
+        raise NotImplementedError, 'SAM file handling is not implemented yet'
 
            
 class CramFile(AlignmentFileBase):
-    def read_header(cls,obj):
-        raise NotImplementedError, 'Cram parsing is not implemented yet'
-    pass
+    _magic = b"\x43\x52\x41\x4d"
+    def __init__(self):
+        raise NotImplementedError, 'Cram file handling is not implemented yet'
 
-def AlignmentFile(filepath):
+def AlignmentFile(filepath,mode='r',**kwargs):
     filepath = filepath
-    fh = open(filepath)
-    magic = fh.read(4)
-    if magic == bgzf._bgzf_magic:
-        return BamFile(filepath)
-    elif magic == _cram_magic:
-        return CramFile(filepath)
-    elif magic == _sam_magic:
-        return SamFile(filepath)
-    else:
-        raise ValueError, 'Unknown format for file %s' % filepath
+    fh = open(filepath,mode)
+    if mode[0] == 'r':
+        magic = fh.read(4)
+        magic_map = {cls._magic:cls for cls in [BamFile,CramFile,SamFile]}
+        if magic not in magic_map:
+            raise ValueError, 'Unknown format for file %s' % filepath
+        return magic_map[magic](filepath,mode,**kwargs)
+    elif mode[0] == 'w':
+        if 'format' not in kwargs:
+            raise ValueError, 'Specify an output format for writing'
+        fmt = kwargs['format']
+        format_map = {cls.__name__[:len(fmt)].lower():cls 
+                      for cls in [BamFile,CramFile,SamFile]}
+        if fmt not in format_map:
+            raise ValueError, 'Unknown format %s' % fmt
+        return format_map[fmt.lower()](filepath,mode,**kwargs)
 
 
 class BamFlag(object):
@@ -353,15 +393,88 @@ class BamFlag(object):
     DUP = 0x400
     SUPPLEMENTARY = 0x800
 
+# calculate bin given an alignment covering [beg,end) (zero-based, half-closed-half-open) 
+# from SAM format specification
+def reg2bin(beg, end):
+    end -= 1
+    if beg>>14 == end>>14: return ((1<<15)-1)/7 + (beg>>14);
+    if beg>>17 == end>>17: return ((1<<12)-1)/7 + (beg>>17);
+    if beg>>20 == end>>20: return ((1<<9)-1)/7 + (beg>>20);
+    if beg>>23 == end>>23: return ((1<<6)-1)/7 + (beg>>23);
+    if beg>>26 == end>>26: return ((1<<3)-1)/7 + (beg>>26);
+    return 0;
+
+# calculate the list of bins that may overlap with region [beg,end) (zero-based) 
+# from SAM format specification
+MAX_BIN=(((1<<18)-1)/7)
+def reg2bins(beg, end):
+    end -= 1
+    bin_list = [0]
+
+    for k in range(1 + (beg>>26), 2 + (end>>26)): bin_list.append(k)
+    for k in range(9 + (beg>>23), 10 + (end>>23)): bin_list.append(k)
+    for k in range(73 + (beg>>20), 74 + (end>>20)): bin_list.append(k)
+    for k in range(585 + (beg>>17), 586 + (end>>17)): bin_list.append(k)
+    for k in range(4681+ (beg>>14), 4682 + (end>>14)): bin_list.append(k)
+
+    return bin_list
+
 def _bam_cigar_type(val):
     return _bam_cigar_type.c>>((val)<<1)&3
 _bam_cigar_type.c = 0x3C1A7
 
 class BamFile(AlignmentFileBase):
-    _seq_enc = '=ACMGRSVTWYHKDBN' 
+    _seq_dec = '=ACMGRSVTWYHKDBN' 
+    _seq_enc = {x:i for i,x in enumerate(list(_seq_dec))}
+    _magic = bgzf._bgzf_magic
+    _header_magic = 'BAM\1'
+
+    _tag_sam_types = 'cCsSiIAf'
+    _tag_pystruct_types = 'bBhHiIcf'
+    _tag_trans = string.maketrans(_tag_sam_types,_tag_pystruct_types)
+    _tag_sizes = {'c':1, 'C':1, 's':2, 'S':2, 'i': 4, 'I': 4, 'A': 1, 'f': 4}
 
     def __iter__(self):
         return self
+
+    def write(self,aln):
+        if not self.write_mode:
+            raise ValueError, 'Not opened for writing'
+        if aln.block is not None and aln._block_format == self._magic:
+            block = aln.block
+        else:
+            seq_len = len(aln.seq)
+            block = struct.pack(
+                'iiiiiiii',
+                aln.tid,
+                aln.pos,
+                reg2bin(aln.pos,aln.ref_alignment_end) << 16 | 
+                    aln.mapq << 8 | (len(aln.qname)+1),
+                aln.flag << 16 | len(aln.cigar),
+                seq_len,
+                aln.mate_tid,
+                aln.mate_pos,
+                aln.tlen) + aln.qname + '\x00'
+
+            for op,op_len in aln.cigar:
+                block += struct.pack('i',op_len << 4 | op)
+            for c1,c2 in itertools.izip(itertools.islice(aln.seq,0,None,2),
+                                        itertools.islice(aln.seq,1,None,2)):
+                block += struct.pack('B', self._seq_enc[c1]<<4 | self._seq_enc[c2])
+            if seq_len % 2:
+                block += struct.pack('B',self._seq_enc[aln.seq[-1]] << 4)
+            #block += ''.join(chr(c) for c in aln.qual)
+            block += struct.pack('b'*seq_len,*aln.qual)
+            for tag in aln.tags:
+                block += tag.tag+tag.val_type
+                if tag.val_type in self._tag_sam_types:
+                    block += tag.value_block
+                elif tag.val_type == 'Z':
+                    block += tag.value + '\0'
+                else:
+                    raise ValueError, 'Tag type %s not supported' % tag.val_type
+
+        self.fh.write(struct.pack('i',len(block)) + block)
 
     def next(self):
         block_size_block = self.fh.read(4)
@@ -372,82 +485,53 @@ class BamFile(AlignmentFileBase):
 
         block = self.fh.read(block_size)
 
-        refID = struct.unpack('i',block[0:4])[0]
-        pos = struct.unpack('i',block[4:8])[0]
-        # note don't know about bin yet--probably need to store or compute this?
-        bin_mq_nl = struct.unpack('I',block[8:12])[0]
+        refID,pos,bin_mq_nl,flag_nc,l_seq,next_refID,next_pos,tlen =\
+            struct.unpack('iiIIiiii',block[:32])
+
         mapq = (bin_mq_nl >> 8) & 0xff
-        flag_nc = struct.unpack('i',block[12:16])[0]
         flag = flag_nc >> 16
-        l_seq = struct.unpack('i',block[16:20])[0]
-        next_refID = struct.unpack('i',block[20:24])[0]
-        next_pos = struct.unpack('i',block[24:28])[0]
-        tlen = struct.unpack('i',block[28:32])[0]
         read_name = block[32:].split('\x00')[0]
         block = block[33+len(read_name):]
-        #while True:
-            #c = self.fh.read(1)
-            #if ord(c) == 0:
-                #break
-            #read_name += c
 
         n_cigar_op = flag_nc & 0xffff
-        raw_cigar = [struct.unpack('I',block[i*4:i*4+4])[0] 
-                 for i in range(n_cigar_op)]
+        cigar_end = n_cigar_op*4
+        raw_cigar = struct.unpack('I'*n_cigar_op,block[:cigar_end])
         cigar = tuple((x&0xf,x>>4) for x in raw_cigar)
 
-        p = n_cigar_op*4
-        enc_seq = [struct.unpack('B',block[p + i])[0] 
-                   for i in range((l_seq+1)/2)]
+        p = cigar_end
+        seq_end = p+(l_seq+1)/2
+        enc_seq = struct.unpack('B'*((l_seq+1)/2),block[p:seq_end])
         seq = ''
         for c in enc_seq:
-            seq += self._seq_enc[c>>4]
+            seq += self._seq_dec[c>>4]
             if len(seq) < l_seq:
-                seq += self._seq_enc[c & 0xf]
+                seq += self._seq_dec[c & 0xf]
 
-        p += (l_seq+1)/2
+        p = seq_end
+        qual_end = p+l_seq
 
-        qual = [struct.unpack('b',block[p+i])[0]
-                for i in range(l_seq)]
+        qual = struct.unpack('b'*l_seq,block[p:qual_end])
 
         remainder = block_size - 32 - len(read_name) - 1 - 4*n_cigar_op -\
                 (l_seq+1)/2 - l_seq
         tag_block = block[p+l_seq:]
-        #tag_block = self.fh.read(remainder)
+
         i = 0
         tags = []
         while i < len(tag_block)-1:
             tag = tag_block[i:i+2]
             val_type = tag_block[i+2]
-            if val_type == 'Z':
-                i += 3
-                value = ''
-                while True:
-                    c = tag_block[i]
-                    i += 1
-                    if ord(c) == 0:
-                        break
-                    value += c
-            elif val_type == 'c' or val_type == 'C':
-                value = struct.unpack(chr(ord(val_type)-1),tag_block[i+3])[0]
-                i += 4
-            elif val_type == 's' or val_type == 'S':
-                value = struct.unpack(chr(ord(val_type)-11),tag_block[i+3:i+5])[0]
-                i += 5
-            elif val_type == 'i' or val_type == 'I':
-                value = struct.unpack(val_type,tag_block[i+3:i+7])[0]
-                i += 7
-            elif val_type == 'A':
-                value = tag_block[i+3]
-                i += 4
-            elif val_type == 'f':
-                value = struct.unpack(val_type,tag_block[i+3:i+7])[0]
-                i += 7
+            if val_type in self._tag_sam_types:
+                value_block = tag_block[i+3:i+3+self._tag_sizes[val_type]]
+            elif val_type == 'Z':
+                value_block = tag_block[i+3:].split('\x00')[0]
             else:
-                raise NotImplementedError, 'Tag type %s not implemented' % val_type
-            tags.append(Tag(tag,val_type,value))
-            return AlignedSegment(self,refID,pos,mapq,flag,next_refID,next_pos,
-                                  tlen,read_name,cigar,seq,qual,tags)
+                raise ValueError, 'Tag type %s not supported' % tag.val_type
+            i += 3+len(value_block)
+            tags.append(SamTag(tag,val_type,value_block=value_block))
+
+        return AlignedSegment(self,refID,pos,mapq,flag,next_refID,next_pos,
+                              tlen,read_name,cigar,seq,qual,tags)
             
         '''
         Z - string
@@ -467,7 +551,7 @@ class BamFile(AlignmentFileBase):
         self.fh = bgzf.open(self.filepath)
         magic = struct.unpack('i',self.fh.read(4))[0]
         l_text = struct.unpack('i',self.fh.read(4))[0]
-        text = self.fh.read(l_text)
+        self.header_text = self.fh.read(l_text)
         n_ref = struct.unpack('i',self.fh.read(4))[0]
         for tid in range(n_ref):
             l_name = struct.unpack('i',self.fh.read(4))[0]
@@ -476,19 +560,64 @@ class BamFile(AlignmentFileBase):
             l_ref = struct.unpack('i',self.fh.read(4))[0]
             self.reference_table[name] = AlignmentReference(tid,name,l_ref)
             self.reference_names.append(name)
+
+    def header_from_template(self,template):
+        self.header_text = template.header_text
+        self.reference_table = template.reference_table
+
+    # XXX: make a single object for the header instead of text and table
+    def header_from_object(self,object):
+        raise NotImplementedError, 'Headers from objects not supported yet'
+
+    def write_header(self):
+        if not hasattr(self,'header_text'):
+            raise ValueError, 'Cannot write a header when none is defined'
+        self.fh = bgzf.BgzfWriter(self.filepath,self.mode,compresslevel=1)
+        block = self._header_magic
+        block += struct.pack('i',len(self.header_text)+1)
+        block += self.header_text + '\0'
+        block += struct.pack('i',len(self.reference_table))
+        #print len(self.reference_table)
+        for name,aref in self.reference_table.items():
+            block += struct.pack('i',len(name)+1)
+            block += name + '\0'
+            block += struct.pack('i',aref.length)
+
+        self.fh.write(block)
+            #print len(name)+1,name,aref.length
+
  
 
 
-if __name__ == '__main__':
+def main():
     import sys
 
-    if len(sys.argv) > 1:
-        fa = FastaFile(sys.argv[1])
-        raise SystemExit
+    #if len(sys.argv) > 1:
+        #fa = FastaFile(sys.argv[1])
+        #raise SystemExit
     if len(sys.argv) > 1:
         af = AlignmentFile(sys.argv[1])
-        for aln in af:
-            print aln.seq,aln.cigar,aln.cigarstring
+        #for aln in af:
+            #print aln.seq,aln.cigar,aln.cigarstring
+        if len(sys.argv) > 2:
+            out = AlignmentFile(sys.argv[2],'wb',format='bam',template=af)
+            for aln in af:
+                out.write(aln)
+            out.fh.close()
+            #af2 = AlignmentFile(sys.argv[2])
+
+            #for (k1,v1),(k2,v2) in zip(af.reference_table.items(),
+                                       #af2.reference_table.items()):
+                #print k1,k2,k1==k2,v1==v2
+            #pprint.pprint(af.reference_table)
+            #pprint.pprint(af2.reference_table)
 
 
 
+if __name__ == '__main__':
+    import cProfile
+    import pstats
+    cProfile.run('main()','tmpstats')
+    p = pstats.Stats('tmpstats')
+    p.strip_dirs().sort_stats('cumtime').print_callees()
+    #main()
